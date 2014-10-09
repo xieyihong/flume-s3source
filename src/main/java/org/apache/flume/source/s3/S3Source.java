ca157bdd27127e14ca1c3093568c974e83960e23
paliwalashish <paliwalashish@gmail.com>
2014-10-09 11:52:17 +0530

    added config param

--- src/main/java/org/apache/flume/source/s3/S3Source.java
+++ src/main/java/org/apache/flume/source/s3/S3Source.java
@@ -19,5 +19,49 @@
 
 package org.apache.flume.source.s3;
 
-public class S3Source {
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import org.apache.flume.Context;
+import org.apache.flume.EventDrivenSource;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.source.AbstractSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class S3Source extends AbstractSource implements
+        Configurable, EventDrivenSource {
+
+  private static final Logger logger = LoggerFactory
+          .getLogger(S3Source.class);
+
+  private String awsAccessKeyId;
+  private String awsSecretKey;
+  private int batchSize;
+
+  // TODO which option to use
+  private String s3Buckets;
+  private String endPoint;
+
+  public static final int DEFAULT_BATCH_SIZE = 1000;
+
+  @Override
+  public void configure(Context context) {
+    awsAccessKeyId = context.getString(S3SourceConstants.AWS_ACCESS_KEY_ID);
+    Preconditions.checkArgument(!Strings.isNullOrEmpty(awsAccessKeyId),
+            "AWS Key Id is required");
+
+    awsSecretKey = context.getString(S3SourceConstants.AWS_SECRET_KEY);
+    Preconditions.checkArgument(!Strings.isNullOrEmpty(awsSecretKey),
+            "AWS Secret Key must be specified");
+
+    s3Buckets = context.getString(S3SourceConstants.S3_BUCKETS);
+    Preconditions.checkArgument(!Strings.isNullOrEmpty(s3Buckets),
+            "Bucket name must be specified");
+
+    endPoint = context.getString(S3SourceConstants.END_POINT);
+    Preconditions.checkArgument(!Strings.isNullOrEmpty(endPoint),
+            "Endpoint cannot be null");
+
+    batchSize = context.getInteger(S3SourceConstants.BATCH_SIZE, DEFAULT_BATCH_SIZE);
+  }
 }
